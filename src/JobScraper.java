c617f3a6a6e57e0ef8cd8f614d347d34fb2d46b3
danderson100 <danderson7790@gmail.com>
2021-02-15 07:46:14 -0700

    modularized scraper class

--- src/JobScraper.java
+++ src/JobScraper.java
@@ -26,8 +26,6 @@ public class JobScraper {
     }
     //TODO: add more websites; add different queries
 
-
-    //TODO: modularize this method
     public void scrapeForJobs(String searchQuery, String site) {
         WebClient client = new WebClient();
         client.getOptions().setCssEnabled(false);
@@ -37,44 +35,23 @@ public class JobScraper {
         while (pageCount < 45) {
 
             String searchPage = "&start=" + pageCount;
-
+            String searchUrl = generateUrl(pageCount, searchPage, searchQuery, site);
             try {
-                String searchUrl = "";
-                if (pageCount < 10) {
-                    searchUrl = siteQueries.get(site)
-                            + URLEncoder.encode(searchQuery, StandardCharsets.UTF_8);
-                } else {
-                    searchUrl = siteQueries.get(site)
-                            + URLEncoder.encode(searchQuery, StandardCharsets.UTF_8) + searchPage;
-                }
 
                 HtmlPage page = client.getPage(searchUrl);
 
-                List<HtmlElement> items = page.getByXPath(".//div[@class='jobsearch-SerpJobCard unifiedRow row result']");
+                List<HtmlElement> items =
+                        page.getByXPath(".//div[@class='jobsearch-SerpJobCard unifiedRow row result']");
+
                 if (items.isEmpty()) {
                     System.out.println("No items found.");
                 } else {
                     for (HtmlElement item : items) {
-                        HtmlElement jobTitle = item.getFirstByXPath(".//h2[@class='title']");
-                        HtmlElement spanCompany = item.getFirstByXPath(".//span[@class='company']");
-                        HtmlElement jobSummary = item.getFirstByXPath(".//div[@class='summary']");
-                        HtmlElement jobLocation =
-                                item.getFirstByXPath(".//span[@class='location accessible-contrast-color-location']");
-                        HtmlElement jobPostDate = item.getFirstByXPath(".//span[@class='date ']");
-
-
-
-                        String jobName = jobTitle == null ? "Couldn't get title" : jobTitle.asText();
-                        //  String jobUrl = jobTitle.getHrefAttribute();
-
-                        String summaryStr = jobSummary.asText();
-                        String jobCompany = spanCompany == null ? "No Company Name" : spanCompany.asText();
-                        String locString = jobLocation == null ? "No Location Found" : jobLocation.asText();
-                        String postDate = jobPostDate == null ? "Couldn't find date" : jobPostDate.asText();
-                        Job webJob = new Job(jobName, jobCompany, locString, postDate, summaryStr);
+                        Job webJob = createJob(item);
                         allJobs.add(webJob);
 
                     }
+
                 }
             } catch (Exception e) {
                 System.out.println("Error: " + e);
@@ -82,6 +59,39 @@ public class JobScraper {
             pageCount += 10;
         }
 
+    }
+
+    private Job createJob(HtmlElement item) {
+
+        HtmlElement jobTitle = item.getFirstByXPath(".//h2[@class='title']");
+        HtmlElement spanCompany = item.getFirstByXPath(".//span[@class='company']");
+        HtmlElement jobSummary = item.getFirstByXPath(".//div[@class='summary']");
+        HtmlElement jobLocation =
+                item.getFirstByXPath(".//span[@class='location accessible-contrast-color-location']");
+        HtmlElement jobPostDate = item.getFirstByXPath(".//span[@class='date ']");
+
+        String jobName = jobTitle == null ? "Couldn't get title" : jobTitle.asText();
+        //  String jobUrl = jobTitle.getHrefAttribute();
+
+        String summaryStr = jobSummary.asText();
+        String jobCompany = spanCompany == null ? "No Company Name" : spanCompany.asText();
+        String locString = jobLocation == null ? "No Location Found" : jobLocation.asText();
+        String postDate = jobPostDate == null ? "Couldn't find date" : jobPostDate.asText();
+
+        return new Job(jobName, jobCompany, locString, postDate, summaryStr);
+    }
+
+    private String generateUrl(int pageCount, String searchPage, String searchQuery, String site) {
+        String searchUrl = "";
+        if (pageCount < 10) {
+            searchUrl = siteQueries.get(site)
+                    + URLEncoder.encode(searchQuery, StandardCharsets.UTF_8);
+        } else {
+            searchUrl = siteQueries.get(site)
+                    + URLEncoder.encode(searchQuery, StandardCharsets.UTF_8) + searchPage;
+        }
+
+        return searchUrl;
     }
 
 }
